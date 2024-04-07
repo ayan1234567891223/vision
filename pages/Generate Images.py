7c33c42c4a89acf30594a8378356dfe6ec0aa849
ayan1234567891223 <ayanshekharonline1@gmail.com>
2024-04-07 19:54:47 +0530

    9th

--- pages/Generate Images.py
+++ pages/Generate Images.py
@@ -1,3 +1,4 @@
+import requests
 import streamlit as st
 from openai import OpenAI
 
@@ -10,17 +11,16 @@ client.api_key = open_ai_key
 client.organization = "org-bghRR0Lowkz6U0iE0yxHIq0a"
 
 def makeimage(prompt: str):
-    response = client.images.generate(
-        model="dall-e-3",
-        prompt=prompt,
-        size="1024x1024",
-        quality="standard",
-        n=1,
-    )
-
+    response = requests.post("https://api.openai.com/v1/images/generations", headers={"Content-Type": "application/json", "Authorization": "Bearer " + open_ai_key}, json={
+        "model": "dall-e-3",
+        "prompt": prompt,
+        "n": 1,
+        "size": "1024x1024"
+    })
+    
     image_url = response.data[0].url
     
-    st.image(image=image_url, caption=prompt)
+    st.image(image_url)
 
 st.subheader("Generate Images Using AI")
 
