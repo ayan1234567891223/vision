b4413c6679e387f6f04a95db41b7ccc2a3f92796
ayan1234567891223 <ayanshekharonline1@gmail.com>
2024-04-07 19:55:19 +0530

    10th

--- pages/Generate Images.py
+++ pages/Generate Images.py
@@ -1,14 +1,10 @@
 import requests
 import streamlit as st
-from openai import OpenAI
 
 st.subheader("Enter Your OpenAI Key To Use All The Features")
 
 open_ai_key = st.text_input("OpenAI Key (Press enter after putting your OpenAI API Key)")
 
-client = OpenAI()
-client.api_key = open_ai_key
-client.organization = "org-bghRR0Lowkz6U0iE0yxHIq0a"
 
 def makeimage(prompt: str):
     response = requests.post("https://api.openai.com/v1/images/generations", headers={"Content-Type": "application/json", "Authorization": "Bearer " + open_ai_key}, json={
